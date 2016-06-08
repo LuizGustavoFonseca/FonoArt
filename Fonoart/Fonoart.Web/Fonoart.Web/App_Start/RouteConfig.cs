f6b668fdd66a8734862c45cf59f1b810f02f63b8
Luiz Gustavo <luiz.gustavo.fon@gmail.com>
2016-06-07 21:03:06 -0300

    a

--- Fonoart/Fonoart.Web/Fonoart.Web/App_Start/RouteConfig.cs
+++ Fonoart/Fonoart.Web/Fonoart.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Fonoart.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Atendimento", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
